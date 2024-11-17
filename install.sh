fdf988e8b39104b5e9393d5173b6ba2fed3abd16
PerilousBooklet <raffaele.orabona@protonmail.com>
2024-11-18 00:03:41 +0100

    Tidied up the manual arch linux instructions for lsp servers.

--- install.sh
+++ install.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/bash
 
+# Arduino
+sudo pacman -Syu arduino-language-server
+
+# Assembly x86_64
+paru -S asm-lsp
+
 # Bash
-sudo pacman -Syu bash-language-server
+sudo pacman -Syu bash-language-server shellcheck
 
 # C/C++
 sudo pacman -Syu clang
+
+# C#/F#
+sudo pacman -Syu dotnet-sdk
+paru -S omnisharp-roslyn-bin
+paru -S fsharp-language-server
 
 # Clojure
 paru -S clojure-lsp-bin
@@ -12,14 +23,12 @@ paru -S clojure-lsp-bin
 # Crystal
 paru -S crytalline-bin
 
-# C#
-paru -S omnisharp-roslyn
-
 # CSS
 sudo pacman -Syu vscode-css-languageserver
 
 # D
-paru -S serve-d
+paru -S serve-d-bin
+sudo pacman -Syu dub
 
 # Dart
 sudo pacman -Syu dart
@@ -31,6 +40,7 @@ paru -S elixir-ls
 paru -S elm-language-server
 
 # Erlang
+# WIP: package still not in the AUR
 paru -S erlang_ls
 
 # Fortran
@@ -47,26 +57,32 @@ sudo pacman -Syu haskell-language-server
 
 # HTML
 sudo pacman -Syu vscode-html-languageserver
+paru -S emmet-language-server
 
 # Java
 paru -S jdtls
-
-# Typescript
-sudo pacman -Syu typescript-language-server
 
 # JSON
 sudo pacman -Syu vscode-json-languageserver
 
 # Julia
-julia && ]add LanguageServer
+# NOTE: open the julia console and install the lsp server
+julia
+# ]
+# add LanguageServer
 
 # Kotlin
 paru -S kotlin-language-server
+
+# Lisp
+sudo pacman -Syu roswell
 
 # Lua
 paru -S lua-language-server-git
 
 # Nim
+paru -S choosenim-bin
+choosenim stable
 nimble install nimlsp
 
 # Ocaml
@@ -79,6 +95,9 @@ opam install user-setup
 # Odin
 paru -S odinls
 
+# OpenSCAD
+paru -S openscad-lsp
+
 # Perl
 paru -S perlnavigator
 
@@ -86,28 +105,39 @@ paru -S perlnavigator
 paru -S nodejs-intelephense
 
 # Python
-sudo pacman -Syu python-lsp-server
+sudo pacman -Syu python-lsp-server flake8
 
 # R
 paru -S r-languageserver
 
 # Ruby
+# NOTE: https://wiki.archlinux.org/title/RVM
 gem install ruby-lsp
 
 # Rust
-lpm install lsp_rust
+lpm install lsp_rust # or sudo pacman -Syu rustup
 
 # Scala
+paru -S jdk17-openjdk scala scala-docs scala-sources
+sudo archlinux-java set java-17-openjdk
 paru -S metals
 
 # SQL
 paru -S sql-language-server
 
-# LaTeX
-paru -S texlab
+# TailwindCSS
+sudo pacman -Syu tailwindcss-language-server
+
+# TeX
+sudo pacman -Syu texlive-most
+paru -S texlab-bin
+sudo pacman -Syu texlive-binextra perl-yaml-tiny perl-file-homedir
 
 # TOML
 sudo pacman -S taplo-cli
+
+# Typescript
+sudo pacman -Syu typescript-language-server
 
 # Typst
 paru -S typst-lsp-bin
